956f4b26650ca809454fd8cbfb903ee6454e2312
najari <amirezanajari@gmail.com>
2023-12-10 09:58:23 +0330

    najari-ref:added borderRadius to all elements

--- app/(sidebar)/(knowledge)/data.js
+++ app/(sidebar)/(knowledge)/data.js
@@ -1,32 +1,41 @@
 export const items = [
-    {
-        text: "Functional components"
-    },
-    {
-        text: "Responsive design"
-    },
-    {
-        text: "Redux Toolkit, Thunk, RTK Query"
-    },
-    {
-        text: "Context API"
-    },
-    {
-        text: "Material UI"
-    },
-    {
-        text: "Tailwind"
-    },
-    {
-        text: "Bootstrap"
-    },
-    {
-        text: "ES6"
-    },
-    {
-        text: "Familiar with Git",
-    },
-    {
-        text: "Familiar with Design Pattern",
-    }
-]
+  {
+    text: "Functional components",
+  },
+  {
+    text: "Responsive design",
+  },
+  {
+    text: "Redux Toolkit, Thunk, RTK Query",
+  },
+  {
+    text: "Context API",
+  },
+  {
+    text: "Material UI",
+  },
+  {
+    text: "Tailwind",
+  },
+  {
+    text: "Bootstrap",
+  },
+  {
+    text: "ES6",
+  },
+  {
+    text: "Axios",
+  },
+  {
+    text: "Familiar with Git",
+  },
+  {
+    text: "Plotly.js",
+  },
+  {
+    text: "Familiar with Docker",
+  },
+  {
+    text: "Familiar with Design Pattern",
+  },
+];
--- app/(sidebar)/(profile)/Profile.jsx
+++ app/(sidebar)/(profile)/Profile.jsx
@@ -14,7 +14,6 @@ const Profile = () => {
                     <AiOutlineMore />
                 </div>
             )}
-
             <Image
                 src="./profile.jpg"
                 alt="amireza najari"
